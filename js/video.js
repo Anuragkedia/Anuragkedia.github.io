ef323709353bade147cd89103c1d7c5843b568dc
Anuragkedia <39305881+Anuragkedia@users.noreply.github.com>
2019-07-23 18:14:46 +0530

    Update video.js

--- js/video.js
+++ js/video.js
@@ -24,3 +24,30 @@ enterButton.addEventListener('click', () => {
   video.pause();
   splash.style.display = 'none';
 });
+
+
+const video1 = document.getElementById('theaterVideo1');
+video1.pause();
+AFRAME.registerComponent('listener1', {
+  tick1: function () {
+    const userPosition = this.el.getAttribute('position')["z"];
+
+    if (userPosition <= -17) {
+      video1.play();
+    } else {
+      video1.pause();
+    }
+  }
+});
+
+// Workaround for mobile video
+// uses the splash screen button to trigger video play/pause so that the
+// video will auto play when the user enters the theater room
+const enterButton1 = document.querySelector('.splash__button')
+const splash1 = document.querySelector('.splash');
+
+enterButton.addEventListener('click', () => {
+  video1.play();
+  video1.pause();
+  splash1.style.display = 'none';
+});
