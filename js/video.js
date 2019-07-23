e0026034e858208514979beb4afd1c26cf9f383c
Anuragkedia <39305881+Anuragkedia@users.noreply.github.com>
2019-07-23 18:20:42 +0530

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,5 +1,5 @@
 // play video when user enters room. Pause when leaving
-const video = document.getElementById('theaterVideo');
+const video = document.getElementById('theaterVideo1');
 video.pause();
 AFRAME.registerComponent('listener', {
   tick: function () {
@@ -26,28 +26,28 @@ enterButton.addEventListener('click', () => {
 });
 
 
-const video1 = document.getElementById('theaterVideo1');
-video1.pause();
-AFRAME.registerComponent('listener1', {
-  tick1: function () {
-    const userPosition = this.el.getAttribute('position')["z"];
+// const video1 = document.getElementById('theaterVideo1');
+// video1.pause();
+// AFRAME.registerComponent('listener1', {
+//   tick1: function () {
+//     const userPosition = this.el.getAttribute('position')["z"];
 
-    if (userPosition <= -17) {
-      video1.play();
-    } else {
-      video1.pause();
-    }
-  }
-});
+//     if (userPosition <= -17) {
+//       video1.play();
+//     } else {
+//       video1.pause();
+//     }
+//   }
+// });
 
-// Workaround for mobile video
-// uses the splash screen button to trigger video play/pause so that the
-// video will auto play when the user enters the theater room
-const enterButton1 = document.querySelector('.splash__button')
-const splash1 = document.querySelector('.splash');
+// // Workaround for mobile video
+// // uses the splash screen button to trigger video play/pause so that the
+// // video will auto play when the user enters the theater room
+// const enterButton1 = document.querySelector('.splash__button')
+// const splash1 = document.querySelector('.splash');
 
-enterButton.addEventListener('click', () => {
-  video1.play();
-  video1.pause();
-  splash1.style.display = 'none';
-});
+// enterButton.addEventListener('click', () => {
+//   video1.play();
+//   video1.pause();
+//   splash1.style.display = 'none';
+// });
