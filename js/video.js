7de3c8cbb86c91bdfb19cef7a4967676d9c16086
Anuragkedia <39305881+Anuragkedia@users.noreply.github.com>
2019-07-25 09:23:11 +0530

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,5 +1,5 @@
 // play video when user enters room. Pause when leaving
-const video = document.getElementById('theaterVideo1');
+const video = document.getElementById('theaterVideo');
 video.pause();
 AFRAME.registerComponent('listener', {
   tick: function () {
@@ -24,30 +24,3 @@ enterButton.addEventListener('click', () => {
   video.pause();
   splash.style.display = 'none';
 });
-
-
-// const video1 = document.getElementById('theaterVideo1');
-// video1.pause();
-// AFRAME.registerComponent('listener1', {
-//   tick1: function () {
-//     const userPosition = this.el.getAttribute('position')["z"];
-
-//     if (userPosition <= -17) {
-//       video1.play();
-//     } else {
-//       video1.pause();
-//     }
-//   }
-// });
-
-// // Workaround for mobile video
-// // uses the splash screen button to trigger video play/pause so that the
-// // video will auto play when the user enters the theater room
-// const enterButton1 = document.querySelector('.splash__button')
-// const splash1 = document.querySelector('.splash');
-
-// enterButton.addEventListener('click', () => {
-//   video1.play();
-//   video1.pause();
-//   splash1.style.display = 'none';
-// });
